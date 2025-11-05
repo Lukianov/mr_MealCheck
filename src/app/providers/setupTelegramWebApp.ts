071fa3095f275d6fd418d5623705633da65b3ddd
lukianov <lukyanovvitaliymailbox@gmail.com>
2025-11-05 10:19:01 +0300

    fix: add cloud storage item for onboarding

--- src/app/providers/setupTelegramWebApp.ts
+++ src/app/providers/setupTelegramWebApp.ts
@@ -1,14 +1,25 @@
 import WebApp from '@twa-dev/sdk'
 import { useApiClient } from '@/shared/api'
+import { RouteName, router } from '@/shared/lib/router'
+
+export const IS_ONBOARDING_PASSED_KEY = 'IS_ONBOARDING_PASSED'
 
 export const setupTelegramWebApp = () => {
   const { setTelegramInitData } = useApiClient()
 
   WebApp.headerColor = '#121212'
-  WebApp.ready()
-  setTelegramInitData(WebApp.initData)
-  WebApp.expand()
 
-  WebApp.BackButton.show()
-  WebApp.BackButton.onClick(() => window.history.back())
+  WebApp.ready()
+
+  setTelegramInitData(WebApp.initData)
+
+  debugger
+
+  const onboardingKey = WebApp.CloudStorage.getItem(IS_ONBOARDING_PASSED_KEY)
+
+  if (!onboardingKey) {
+    void router.push({ name: RouteName.Onboarding })
+  }
+
+  WebApp.expand()
 }
--- src/features/onboarding/model/useOnboardingPage.ts
+++ src/features/onboarding/model/useOnboardingPage.ts
@@ -1,6 +1,8 @@
 import { ref } from 'vue'
 import { RouteName, router } from '@/shared/lib/router'
 import { useGoalSelection } from '@/features/onboarding/model/useGoalSelection'
+import WebApp from '@twa-dev/sdk'
+import { IS_ONBOARDING_PASSED_KEY } from '@/app/providers/setupTelegramWebApp'
 
 export const useOnboarding = () => {
   const currentSlideIndex = ref(0)
@@ -11,6 +13,8 @@ export const useOnboarding = () => {
     if (currentSlideIndex.value === 5) {
       try {
         void sendOnboardingCharacteristics()
+
+        WebApp.CloudStorage.setItem(IS_ONBOARDING_PASSED_KEY, 'true')
       } catch (e) {
         console.error(e)
       }
