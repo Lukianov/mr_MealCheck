c9efa07876ad3b33e74073c9e717d2ac6eabb902
lukianov <lukyanovvitaliymailbox@gmail.com>
2025-11-12 23:19:27 +0300

    feat: hide take photo button

--- src/features/log-meal/ui/LogMealActionSheet.vue
+++ src/features/log-meal/ui/LogMealActionSheet.vue
@@ -7,30 +7,31 @@
         border: '1px solid rgba(255, 255, 255, 0.08)',
       }"
     >
-      <button
-        class="w-full flex items-center justify-between gap-3 active:scale-[1.1] py-1.5"
-        @click="openCamera"
-      >
-        <span class="flex items-center gap-3">
-          <span class="flex w-7 h-7 items-center justify-center">
-            <CameraIcon />
-          </span>
-          <span class="text-left min-w-36 whitespace-nowrap text-base">
-            {{ ru.logMealWidget.takePhoto }}
-          </span>
-        </span>
-        <RightArrowIcon class="w-4 h-4 shrink-0" />
-      </button>
+      <!--      <button-->
+      <!--        class="w-full flex items-center justify-between gap-3 active:scale-[1.1] py-1.5"-->
+      <!--        @click="openCamera"-->
+      <!--      >-->
+      <!--        <span class="flex items-center gap-3">-->
+      <!--          <span class="flex w-7 h-7 items-center justify-center">-->
+      <!--            <CameraIcon />-->
+      <!--          </span>-->
+      <!--          <span class="text-left min-w-36 whitespace-nowrap text-base">-->
+      <!--            {{ ru.logMealWidget.takePhoto }}-->
+      <!--          </span>-->
+      <!--        </span>-->
+      <!--        <RightArrowIcon class="w-4 h-4 shrink-0" />-->
+      <!--      </button>-->
       <button
         class="w-full flex items-center justify-between gap-3 rounded-2xl active:scale-[1.1] py-1.5"
         @click="openGallery"
       >
         <span class="flex items-center gap-3">
           <span class="flex w-7 h-7 items-center justify-center">
-            <GalleryIcon />
+            <CameraIcon />
+            <!--            <GalleryIcon />-->
           </span>
           <span class="text-left min-w-36 text-base whitespace-nowrap">
-            {{ ru.logMealWidget.fromGallery }}
+            {{ ru.logMealWidget.takePhoto }}
           </span>
         </span>
         <RightArrowIcon class="w-4 h-4 shrink-0" />
@@ -73,7 +74,7 @@ import { ref } from 'vue'
 import WebApp from '@twa-dev/sdk'
 import RightArrowIcon from '@/shared/assets/icons/right-arrow-icon.svg'
 import CameraIcon from '@/shared/assets/icons/camera-icon.svg'
-import GalleryIcon from '@/shared/assets/icons/gallery-icon.svg'
+// import GalleryIcon from '@/shared/assets/icons/gallery-icon.svg'
 import TextInputIcon from '@/shared/assets/icons/text-input-icon.svg'
 import { ru } from '@/shared/lib/i18n/ru'
 
@@ -93,11 +94,11 @@ function haptic() {
   WebApp.HapticFeedback.impactOccurred('light')
 }
 
-function openCamera() {
-  haptic()
-
-  cameraInput.value?.click()
-}
+// function openCamera() {
+//   haptic()
+//
+//   cameraInput.value?.click()
+// }
 
 function openGallery() {
   haptic()
